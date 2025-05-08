7fea354bf646201217bde0c2ee98196e1674d865
maybeenang <muhammad.120140194@student.itera.ac.id>
2025-05-09 00:03:32 +0700

    feat: cp

--- CPDEV/android/app/src/main/java/com/cpportal/MainActivity.kt
+++ CPDEV/android/app/src/main/java/com/cpportal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cpdev
+package com.cpportal
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -13,7 +13,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "CPDEV"
+  override fun getMainComponentName(): String = "Cpportal"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- CPDEV/android/app/src/main/java/com/cpportal/MainApplication.kt
+++ CPDEV/android/app/src/main/java/com/cpportal/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cpdev
+package com.cpportal
 
 import android.app.Application
 import com.facebook.react.PackageList
